18c4241885e7c4b1f91ba158d93d8dc78e3ff851
hemanth <khemanthsura@gmail.com>
2025-09-14 22:48:02 +0200

    Final commit
    Parsing the result according to the operation from the server and
    sending the result back to the server and printing its response.

--- clientmain.cpp
+++ clientmain.cpp
@@ -24,12 +24,11 @@ static int readmsg(int sfd, char *buf, size_t buflen) {
     ssize_t n;
     while (i < buflen -1) {
         n = recv(sfd, &s, 1, 0);
-        if(n > 0 ) {
-            DBG("Data Received: %c", s);
-        }else{
-            DBG("Error occurred while receiving");
-        }
-        buf[i++] =   s;
+        if(n <= 0 )
+            break;
+        else if (s == '\n')
+            break;
+        buf[i++] = s;
     }
     buf[i] = '\0';
     return (int)i;
@@ -86,7 +85,7 @@ int err = getaddrinfo(Desthost, buf, &hints, &res);
 
     char msg[128];
     int supported = 0;
-    while (readmsg(sockfd, msg, 128) > 0) {
+    while (readmsg(sockfd, msg, sizeof(msg)) > 0) {
         DBG("Server %s", msg);
         if(strcmp(msg, "TEXT TCP 1.0") == 0)
             supported = 1;
@@ -107,14 +106,76 @@ int err = getaddrinfo(Desthost, buf, &hints, &res);
         close(sockfd);
         return 1;
     }
-
+    
+    printf("ASSIGNMENT: %s\n", msg);
+    
     char operation[16];
     char value1[16];
     char value2[16];
     if(sscanf(msg,"%s %s %s", operation, value1, value2) != 3) {
-        printf("ERROR: During reading values %s", msg);
+        printf("ERROR: During reading values %s\n", msg);
         close(sockfd);
         return 1;
     }
+
+    int ires = 0;
+    float fres = 0.0;
+    int isint = 0;
+    int isfloat = 0.0;
+    if (strcmp(operation, "add") == 0 || strcmp(operation, "sub") == 0 || strcmp(operation, "mul") == 0 || strcmp(operation, "div") == 0){
+        isint = 1;
+        int a = atoi(value1);
+        int b = atoi(value2);
+        if (strcmp(operation, "add") == 0)
+            ires = a + b;
+        else if(strcmp(operation, "sub") == 0)
+            ires = a - b;
+        else if(strcmp(operation, "mul") == 0)
+            ires = a * b;
+        else if(strcmp(operation, "div") == 0) {
+            if(b != 0)
+                ires = a/b;
+            else
+                ires = 0;
+        }
+    } else if(strcmp(operation, "fadd") == 0 || strcmp(operation, "fsub") == 0 || strcmp(operation, "fmul") == 0 || strcmp(operation, "fdiv") == 0){
+        isfloat = 1;
+        float a = atof(value1);
+        float b = atof(value2);
+        if (strcmp(operation, "fadd") == 0)
+            fres = a + b;
+        else if(strcmp(operation, "fsub") == 0)
+            fres = a - b;
+        else if(strcmp(operation, "fmul") == 0)
+            fres = a * b;
+        else if(strcmp(operation, "fdiv") == 0) {
+            if(b != 0.0)
+                fres = a/b;
+            else
+                fres = 0.0;
+        }
+    } else {
+        printf("ERROR: UNKOWN OPERATION\n");
+        close(sockfd);
+        return 1;
+    }
+    if(isint)
+        DBG("Result: %d", ires);
+    else if(isfloat)
+        DBG("Result: %8.8g", fres);
+
+    char result[16];
+    if (isint)
+        snprintf(result, 16, "%d\n", ires);
+    else if(isfloat)
+        snprintf(result, 16, "%8.8g\n", fres);
+
+    send(sockfd, result, strlen(result), 0);
+
+    if(readmsg(sockfd, msg, sizeof(msg)) > 0) {
+        printf("%s (myresult=%s)\n", msg, result);
+    }
+    close(sockfd);
+    return 0;
 
 }
