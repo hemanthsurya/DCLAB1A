e7146868c64d3573d161e895f4049188bfe740ed
hemanth <khemanthsura@gmail.com>
2025-09-22 22:40:03 +0200

    Socket setup
    The server is setup to listen on the provided port along with upto 5
    clients queuing.

--- servermain.cpp
+++ servermain.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
@@ -40,7 +41,37 @@ int main(int argc, char *argv[]){
     #endif
     return 1;
   }
-  
+  int listen_fd = -1;
+  for (rp = res; rp; rp = rp->ai_next) {
+    listen_fd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
+    if (listen_fd == -1) 
+      continue;
+    int i = 1;
+    setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, &i, sizeof(i));
+    if (bind(listen_fd, rp->ai_addr, rp->ai_addrlen) == 0)
+      break;
+    close(listen_fd);
+    listen_fd = -1;
+  }
+  freeaddrinfo(res);
+
+  if (listen_fd == -1) {
+    #ifdef DEBUG  
+      printf("ERROR: BIND FAILED");
+    #endif
+    return 1;
+  }
+
+  if (listen(listen_fd, 5) < 0) {
+    #ifdef DEBUG  
+      printf("ERROR: LISTEN FAILED");
+    #endif
+    close(listen_fd);
+    return 1; 
+  }
+
+  printf("Server listening on %s:%d\n", Desthost, port);
+
 #ifdef DEBUG  
   printf("Host %s, and port %d.\n",Desthost,port);
 #endif
