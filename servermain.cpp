50c9467d6461e4832d1632d35badc95f78246a53
hemanth <khemanthsura@gmail.com>
2025-09-22 21:28:35 +0200

    Intitial commit for the server
    Input parsing and host resolution is implemented.

--- servermain.cpp
+++ servermain.cpp
@@ -1,14 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-/* You will to add includes here */
-
-
-// Included to get the support library
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
 #include <calcLib.h>
 
-// Enable if you want debugging to be printed, see examble below.
-// Alternative, pass CFLAGS=-DDEBUG to make, make CFLAGS=-DDEBUG
 #define DEBUG
 
 
@@ -16,19 +13,34 @@ using namespace std;
 
 
 int main(int argc, char *argv[]){
+  if (argc != 2) {
+    printf("Usage: %s <IP-or-DNS:PORT>\n", argv[0]);
+    return 1;
+  }
   
-  /*
-    Read first input, assumes <ip>:<port> syntax, convert into one string (Desthost) and one integer (port). 
-     Atm, works only on dotted notation, i.e. IPv4 and DNS. IPv6 does not work if its using ':'. 
-  */
   char delim[]=":";
   char *Desthost=strtok(argv[1],delim);
   char *Destport=strtok(NULL,delim);
-  // *Desthost now points to a sting holding whatever came before the delimiter, ':'.
-  // *Dstport points to whatever string came after the delimiter. 
-
-  /* Do magic */
   int port=atoi(Destport);
+  if (!Desthost || !Destport) {
+    printf("Wrong input arguments\n");
+    return 1;
+  }
+
+  struct addrinfo hints, *res, *rp;
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_family   = AF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_flags    = AI_PASSIVE;
+
+  int err = getaddrinfo(Desthost, Destport, &hints, &res);
+  if (err != 0) {
+    #ifdef DEBUG 
+    printf("getaddrinfo\n");
+    #endif
+    return 1;
+  }
+  
 #ifdef DEBUG  
   printf("Host %s, and port %d.\n",Desthost,port);
 #endif
